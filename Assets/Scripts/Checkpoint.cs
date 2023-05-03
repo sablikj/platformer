d5f1e43c4c473b3afc5221c8eefe8cc9a206669d
David Bilnica <58438021+dbilnica@users.noreply.github.com>
2023-05-03 22:47:08 +0100

    Added very great Respawn & Checkpoint System

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour
+{
+    public SpriteRenderer SR;
+    public Sprite cpOn, cpOff;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            CheckpointController.instance.DeactivateCheckpoints();
+            SR.sprite = cpOn;
+
+            CheckpointController.instance.SetSpawnPoint(transform.position);
+        }
+
+    }
+    public void ResetCheckpoint()
+    {
+        SR.sprite = cpOff;
+    }
+}
