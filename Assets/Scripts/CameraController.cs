7da6dbd093d1daf4fb391839c60ce7b66aebee27
Jan Sáblík <j_sablik@utb.cz>
2023-05-03 01:58:04 +0100

    Added Camera controller

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform target;
+
+    public Transform farBackground, middleBackground;
+    public float minHeight, maxHeight;
+    private Vector2 lastPos;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        lastPos = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Updating position of the player, the Y value is scaled between min and max height
+        transform.position = new Vector3(target.position.x, Mathf.Clamp(target.position.y, minHeight, maxHeight), transform.position.z);
+
+        // Moving background
+        Vector2 amountToMove = new Vector2(transform.position.x - lastPos.x, transform.position.y - lastPos.y);
+        farBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f);
+        middleBackground.position += new Vector3(amountToMove.x, amountToMove.y, 0f) * .5f;
+
+        lastPos = transform.position;
+    }
+}
